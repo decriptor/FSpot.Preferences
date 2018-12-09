c59d09a8bb3a6c6c34d294e639be5cee40c88231
Stephen Shaw <sshaw@decriptor.com>
2018-12-08 22:09:42 -0700

    Basic stub code

---
+++ FSpot.Preferences/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace FSpot.Preferences
+{
+    public interface IPrefencesStore
+    {
+        bool TryGet<T>(string key, out T val);
+        void Set(string key, object value); 
+    }
+
+    public class DictionaryPreferenceStore : IPrefencesStore
+    {
+        readonly Dictionary<string, object> store = new Dictionary<string, object>();
+
+        public bool TryGet<T>(string key, out T val)
+        {
+            if (store.ContainsKey(key))
+            {
+                val = (T)store[key];
+                return true;
+            }
+
+            val = null;
+            return false;
+        }
+
+        public void Set(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
